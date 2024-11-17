92026956c05903d71877280f23889f193ef1d762
Zeeboi00 <iwualazolachukwuahu@gmail.com>
2024-11-17 23:03:06 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,63 @@
+
+#calculator for the area of a trapezium
+#a,b and h are variables
+
+print("area of trapezium")
+print("1/2(a+b)xh")
+
+a= float(input("the length=: "))
+b= float(input("the breadth=: "))
+h= float(input("the height=: "))
+
+a_b= a+b
+print("The area of the trapezium=")
+print((0.5*a_b)*h)
+
+#formula for area of a triangle
+#b and h are variables
+
+print("area of a triangle")
+print("1/2(b*h")
+
+b= float(input("the breadth=: "))
+h= float(input("the height=: "))
+
+print("the area of the triangle=")
+print((0.5*b*h))
+
+#formula for circumference of a circle
+#r is a variable
+
+print("circumference of a circle")
+print("2*3.142*r")
+
+r= float(input("the radius=: "))
+
+#formula for volume of a cylinder
+#r and h are variables
+
+print("volume of a cylinder")
+print("3.142*r*r*h")
+
+z= float(input("the radius=: "))
+h= float(input("the height=: "))
+
+print("volume of a cylinder=")
+print("3.142*r*r*h")
+
+q = float(input("the radius=: "))
+m = float(input("the height=: "))
+
+#formula for perimeter of a rectangle
+#l and w are variables
+
+print("perimeter of a rectangle")
+print("2l+2w")
+
+l= float(input("the length=: "))
+w= float(input("the width=: "))
+
+print("perimeter of a rectangle=")
+print(2*l+2*w)
+
+
